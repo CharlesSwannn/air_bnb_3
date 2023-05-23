26512b8987e39692b9bb1ef91c64f139b6b3e7fb
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-05-23 09:06:11 +0100

    created restaurant and review model controller vie

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -14,4 +21,5 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
+  resources :restaurants
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_154038) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_080323) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "details"
